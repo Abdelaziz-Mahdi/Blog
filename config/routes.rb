3db9b7860b6cf7eb3bc042335881adab92643bed
Abdelaziz-Mahdi <abdelaziz.dv@gmail.com>
2024-04-19 14:15:22 +0200

    add api namespace to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

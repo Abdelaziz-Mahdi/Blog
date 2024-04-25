99c28b87b4d3f6a3baf4eab51f428ac37be2e699
Abdelaziz-Mahdi <abdelaziz.dv@gmail.com>
2024-04-25 20:36:11 +0200

    Add PostsDetails component to ror-front

---
+++ ror-front/src/features/posts/PostsDetails.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import { useParams, useNavigate, Link } from 'react-router-dom'
+import { API_URL } from '../../constants'
+
+export default function PostsDetails() {
+    const [post, setPost] = useState({})
+    const navigate = useNavigate()
+    const { id } = useParams()
+    
+    useEffect(() => {
+        fetch(`${API_URL}/${id}`)
+        .then((response) => response.json())
+        .then((data) => setPost(data))
+    }, [id])
+    
+    const handleDelete = () => {
+        fetch(`${API_URL}/${id}`, {
+        method: 'DELETE',
+        })
+        .then(() => navigate('/'))
+    }
+
+    return (
+        <div>
+            {!post.title ? (
+                <div>Loading...</div>
+            ) : (
+                <div>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                    <Link to={`/posts/${id}/edit`}>Edit</Link>
+                    <button onClick={handleDelete}>Delete</button>
+                </div>
+            )
+            }
+            <Link to="/">Back</Link>
+        </div>
+    )
+}

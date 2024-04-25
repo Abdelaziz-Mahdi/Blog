e8f4f68d8ffe98bd267bd3dc407c9a917095e1f6
Abdelaziz-Mahdi <abdelaziz.dv@gmail.com>
2024-04-25 20:35:59 +0200

    Add AppRoutes component to handle routing

---
+++ ror-front/src/components/AppRoutes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import PostsList from "../features/posts/PostsList";
+import PostsForum from "../features/posts/PostsForum";
+import PostsDetails from "../features/posts/PostsDetails";
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<PostsList />} />
+      <Route path="/posts/:id" element={<PostsDetails />} />
+      <Route path="/new" element={<PostsForum />} />
+    </Routes>
+  );
+}

77a8685283962e6da2f2de3f1f909d3819f3b94e
Abdelaziz-Mahdi <abdelaziz.dv@gmail.com>
2024-04-25 20:35:39 +0200

    Refactor App.jsx to use AppRoutes component

--- ror-front/src/App.jsx
+++ ror-front/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import './App.css'
-import PostsList from './features/posts/PostsList'
+import AppRoutes from './components/AppRoutes'
 
 function App() {
 
@@ -11,7 +11,7 @@ function App() {
       <div className='start'>
         <h1>React on Rails</h1>
         <p>find this app</p>
-        <PostsList />
+        <AppRoutes />
       </div>
     </Router>
   )

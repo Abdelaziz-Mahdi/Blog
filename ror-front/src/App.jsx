dad3dc5676f294381b0d8c3f6bb8989368dd5590
Abdelaziz-Mahdi <abdelaziz.dv@gmail.com>
2024-04-25 17:20:15 +0200

    Update App.jsx to use BrowserRouter and NavBar

--- ror-front/src/App.jsx
+++ ror-front/src/App.jsx
@@ -1,16 +1,19 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import NavBar from './components/NavBar'
 import './App.css'
 import PostsList from './features/posts/PostsList'
 
 function App() {
 
   return (
-    <>
-    <div className='start'>
-      <h1>React on Rails</h1>
-      <p>find this app</p>
-      <PostsList />
-    </div>
-    </>
+    <Router>
+      <NavBar />
+      <div className='start'>
+        <h1>React on Rails</h1>
+        <p>find this app</p>
+        <PostsList />
+      </div>
+    </Router>
   )
 }
 

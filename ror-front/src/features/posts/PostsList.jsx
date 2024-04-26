d5ec30facec4b2551fccdfa97ced2e869be39e88
Abdelaziz-Mahdi <abdelaziz.dv@gmail.com>
2024-04-26 16:32:56 +0300

    Add link to post details in PostsList.jsx

--- ror-front/src/features/posts/PostsList.jsx
+++ ror-front/src/features/posts/PostsList.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { API_URL } from "../../constants";
 
 function PostsList(){
@@ -31,7 +32,11 @@ function PostsList(){
         <div>{error}</div>
         {posts.map((post)=>(
             <div key={post.id} className="post-container">
-                <h2>{post.title}</h2>
+                <h2>
+                    <Link to={`/posts/${post.id}`} className="post-title">
+                        {post.title}
+                    </Link>
+                </h2>
                 <p>{post.body}</p>
             </div>
         ))}
